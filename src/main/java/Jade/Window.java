a72f0a2e9035e6731371b0e576f7d52f077b96e2
Joshua Terry <joshterry1990@live.com>
2022-08-19 16:31:48 -0500

    Added Mouse Input support. Next will be the Keyboard support.

--- src/main/java/Jade/Window.java
+++ src/main/java/Jade/Window.java
@@ -4,22 +4,26 @@ import org.lwjgl.Version;
 import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.opengl.GL;
 
+import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.system.MemoryUtil.NULL;
 
-public class Window {
+public class Window
+{
     private int width, height;
     private String title;
     private long glfwWindow;
     private static Window window = null;
 
-    private Window() {
+    private Window()
+    {
         this.height = 1920;
         this.width = 1080;
-        this.title = "Jade";
+        this.title = "Brain Juice";
     }
-    public static Window get() {
+    public static Window get()
+    {
         if (Window.window == null) {
             Window.window = new Window();
         }
@@ -27,22 +31,32 @@ public class Window {
         return Window.window;
     }
 
-    public void run() {
+    public void run()
+    {
         System.out.println("Hello LWJGL " + Version.getVersion() + "!");
 
         init();
         loop();
+
+        // free the memory at end of loop
+        glfwFreeCallbacks(glfwWindow);
+        glfwDestroyWindow(glfwWindow);
+
+        // Terminate GLFW and free the error callback
+        glfwTerminate();
+        glfwSetErrorCallback(null).free();
     }
 
-    public void init() {
+    public void init()
+    {
         // Setup an error callback
         GLFWErrorCallback.createPrint(System.err).set();
 
         // Initialize GLFW
-        if (!glfwInit()) {
+        if (!glfwInit())
+        {
             throw new IllegalStateException("Unable to initialize GLFW.");
         }
-
         // Configure GLFW
         glfwDefaultWindowHints();
         glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
@@ -51,9 +65,15 @@ public class Window {
 
         // Create the window
         glfwWindow = glfwCreateWindow(this.width, this.height, this.title, NULL, NULL);
-        if ( glfwWindow == NULL) {
+        if ( glfwWindow == NULL)
+        {
             throw new IllegalStateException("Failed to create the GLFW Window");
         }
+
+        // Calls for mouseInputs into Window
+        glfwSetCursorPosCallback(glfwWindow, MouseListener::mousePosCallback); // Checks Mouse position
+        glfwSetMouseButtonCallback(glfwWindow, MouseListener::mouseButtonCallback); // Checks mouse button
+        glfwSetScrollCallback(glfwWindow, MouseListener::mouseScrollCallback);
 
         // Make the OpenGL context current
         glfwMakeContextCurrent(glfwWindow);
@@ -71,12 +91,14 @@ public class Window {
         GL.createCapabilities();
     }
 
-    public void loop() {
-        while (!glfwWindowShouldClose(glfwWindow)) {
+    public void loop()
+    {
+        while (!glfwWindowShouldClose(glfwWindow))
+        {
             // poll events
             glfwPollEvents();
 
-            glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+            glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
             glClear(GL_COLOR_BUFFER_BIT);
 
             glfwSwapBuffers(glfwWindow);
