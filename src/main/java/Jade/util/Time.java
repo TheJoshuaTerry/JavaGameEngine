1709c072aef4b5c4f21ae4f420d643806f40e42b
Joshua Terry <joshterry1990@live.com>
2022-08-19 21:03:07 -0500

    Added DeltaTime and Scene Managers.

---
+++ src/main/java/Jade/util/Time.java
@@ -0,0 +1,12 @@
+package Jade.util;
+
+public class Time
+{
+    public static float timeStarted = System.nanoTime();
+
+    public static float getTime()
+    {
+        return (float)((System.nanoTime() - timeStarted) * 1e-9);
+    }
+
+}
